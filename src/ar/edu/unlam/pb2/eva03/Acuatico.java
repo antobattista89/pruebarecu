061111f103b7318cbc9221a4bc9b1cdee0d4fb42
ANTO <anbattista@alumno.unlam.edu.ar>
2021-05-30 22:18:40 -0300

    2da prueba

--- src/ar/edu/unlam/pb2/eva03/Acuatico.java
+++ src/ar/edu/unlam/pb2/eva03/Acuatico.java
@@ -1,7 +1,7 @@
 package ar.edu.unlam.pb2.eva03;
 
 public class Acuatico extends Vehiculo implements IAcuatico{
-
+		//test Anto cambio
 	private Double profundidad;
 	
 	Acuatico(Integer codigo, String nombre) {
